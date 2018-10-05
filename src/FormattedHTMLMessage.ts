e523d2d6e86270716236185b54739805bd7d0520
Sönke Behrendt <soenke.behrendt@gmail.com>
2018-10-05 16:35:25 +0200

    Add support for server side rendering

--- src/FormattedHTMLMessage.ts
+++ src/FormattedHTMLMessage.ts
@@ -1,21 +1,19 @@
-import {createElement} from 'react';
-import {
-    FormattedHTMLMessage as IntlFormattedHTMLMessage,
-} from 'react-intl';
+import { createElement } from 'react';
+import { FormattedHTMLMessage as IntlFormattedHTMLMessage } from 'react-intl';
+import { escapeId, isPhraseEnabled } from './functions';
 
-import {escapeId, isPhraseEnabled} from './functions'
 
 export class FormattedHTMLMessage extends IntlFormattedHTMLMessage {
     constructor(props: any, context: any) {
         super(props, context);
     }
 
-    render() {  
+    render() {
         if (isPhraseEnabled()) {
             let nodes = [escapeId(this.props.id)];
             return createElement("span", null, ...nodes);
         } else {
             return super.render();
-        }    
+        }
     }
 }
--- src/functions.ts
+++ src/functions.ts
@@ -1,38 +1,44 @@
 let phraseAppEditor = false;
 
-function sanitizeConfig(config: any) : any {
+function sanitizeConfig(config: any): any {
   config.prefix = config.prefix ? config.prefix : '{{__';
   config.suffix = config.suffix ? config.suffix : '__}}';
 
   return config;
 }
 
-export function initializePhraseAppEditor (config: any) {
+export function initializePhraseAppEditor(config: any) {
   if (phraseAppEditor) return;
-  
+
   phraseAppEditor = true;
-  (<any>window).PHRASEAPP_ENABLED = config.phraseEnabled;  
-  (<any>window).PHRASEAPP_CONFIG = sanitizeConfig(config);
+
+  if (typeof window === 'undefined') {
+    (<any>global).PHRASEAPP_ENABLED = config.phraseEnabled;
+    (<any>global).PHRASEAPP_CONFIG = sanitizeConfig(config);
+  } else {
+    (<any>window).PHRASEAPP_ENABLED = config.phraseEnabled;
+    (<any>window).PHRASEAPP_CONFIG = sanitizeConfig(config);
+  }
 
   if (config.phraseEnabled) {
-      const phraseapp = document.createElement('script');
-      phraseapp.type = 'text/javascript';
-      phraseapp.async = true;
-      phraseapp.src = ['https://', 'phraseapp.com/assets/in-context-editor/2.0/app.js?', new Date().getTime()].join('');
-      var s = document.getElementsByTagName('script')[0];
-      if (s != undefined) {
-        s.parentNode.insertBefore(phraseapp, s);
-      } else {
-        document.insertBefore(phraseapp, null);
-      }
+    const phraseapp = document.createElement('script');
+    phraseapp.type = 'text/javascript';
+    phraseapp.async = true;
+    phraseapp.src = ['https://', 'phraseapp.com/assets/in-context-editor/2.0/app.js?', new Date().getTime()].join('');
+    var s = document.getElementsByTagName('script')[0];
+    if (s != undefined) {
+      s.parentNode.insertBefore(phraseapp, s);
+    } else {
+      document.insertBefore(phraseapp, null);
     }
+  }
 }
 
-export function isPhraseEnabled() : boolean {
-  return (<any>window).PHRASEAPP_ENABLED
+export function isPhraseEnabled(): boolean {
+  return (typeof window === 'undefined') ? (<any>global).PHRASEAPP_ENABLED : (<any>window).PHRASEAPP_ENABLED
 }
 
-export function escapeId (id : string) : string {
-  let config = (<any>window).PHRASEAPP_CONFIG;
-  return  config.prefix + 'phrase_' + id + config.suffix;
+export function escapeId(id: string): string {
+  const config = (typeof window === 'undefined') ? (<any>global).PHRASEAPP_CONFIG : (<any>window).PHRASEAPP_CONFIG;
+  return config.prefix + 'phrase_' + id + config.suffix;
 }
