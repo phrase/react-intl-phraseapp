67bb6d6c6dc2358e3a10fe8fbc98e7dcf3ce8413
Flávio H Freitas <flaviohenriquecbc@gmail.com>
2025-09-05 15:47:10 +0200

    chore(SCM-747) Update dependencies (#118)

--- helper/test_utils.tsx
+++ helper/test_utils.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react'
-import {IntlProvider} from 'react-intl';
-
-const renderer = require('react-test-renderer');
+import { IntlProvider } from 'react-intl'
+import { render } from '@testing-library/react'
 
 export function setPhraseConfig() {
     Object.defineProperty(window, "PHRASEAPP_CONFIG", {
-    writable: true,
-    value: {
-        prefix: '{{__',
-        suffix: "__}}"
-    }
+        writable: true,
+        value: {
+            prefix: '{{__',
+            suffix: "__}}"
+        }
     });
     Object.defineProperty(window, "PHRASEAPP_ENABLED", {
         writable: true,
@@ -24,9 +23,12 @@ export function disablePhrase() {
     });
 }
 
-export const createComponentWithIntl = (children : any, props: {locale: string, messages?: Record<string, string>} = { locale: 'en' }) => {
-    return renderer.create(
-        <IntlProvider textComponent="span" {...props}>
+export const createComponentWithIntl = (
+    children: React.ReactNode,
+    props: { locale: string, messages?: Record<string, string> } = { locale: 'en' }
+) => {
+    return render(
+        <IntlProvider {...props}>
             {children}
         </IntlProvider>
     );
