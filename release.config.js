4c95c721daf1ea6f95be2bde18ea39a883a57e33
Flavio Freitas <flaviohenriquecbc@gmail.com>
2025-09-25 18:46:32 +0200

    fix(config): lint

--- release.config.js
+++ release.config.js
@@ -9,9 +9,7 @@ module.exports = {
       }],
       "@semantic-release/release-notes-generator",
       "@semantic-release/changelog",
-      ["@semantic-release/npm", {
-          "tarballDir": "release"
-      }],
+      ["@semantic-release/npm", { "tarballDir": "release" }],
       "@semantic-release/github",
       ["@semantic-release/git", {
         "assets": ["package.json"]
